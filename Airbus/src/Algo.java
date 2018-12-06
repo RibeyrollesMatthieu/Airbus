e7f674ff2ad882f3432165559b54410b65b1756c
Dalfrak <Dalfrak@10.0.0.113>
2018-12-06 22:28:46 +0100

    OuiBis

--- Airbus/src/Algo.java
+++ Airbus/src/Algo.java
@@ -1,31 +1,37 @@
-import java.awt.Color;
-
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.image.PixelReader;
+import javafx.scene.paint.Color;
 
 public class Algo {
 	
 	public Image createImage(Image img, Correspondance[] corresp) {
-		ImageView baseIV = new ImageView();
-		baseIV.setImage(img);
+		Traitement tr = new Traitement();
+		Image baseBW = tr.setAsBW(img);
+		ImageView baseIV = new ImageView(baseBW);
+		int baseBWHeight = (int) baseBW.getHeight();
+		int baseBWWidth = (int) baseBW.getWidth();
 		
-		PixelReader pixelReader = img.getPixelReader();
-        System.out.println("Image Width: "+img.getWidth());
-        System.out.println("Image Height: "+img.getHeight());
-        System.out.println("Pixel Format: "+pixelReader.getPixelFormat());
+		PixelReader pixelReader = baseBW.getPixelReader();
+//        System.out.println("Image Width: "+img.getWidth());
+//        System.out.println("Image Height: "+img.getHeight());
+//        System.out.println("Pixel Format: "+pixelReader.getPixelFormat());
 		
-		for (int readY = 0; readY < img.getHeight(); readY++) {
-            for (int readX = 0; readX < img.getWidth(); readX++) {
+		for (int readY = 0; readY < baseBWHeight; readY++) {
+            for (int readX = 0; readX < baseBWWidth; readX++) {
                 Color color = pixelReader.getColor(readX, readY);
-                System.out.println("\nPixel color at coordinates ("
-                        + readX + "," + readY + ") "
-                        + color.toString());
-                System.out.println("R = " + color.getRed());
-                System.out.println("G = " + color.getGreen());
-                System.out.println("B = " + color.getBlue());
-                System.out.println("Opacity = " + color.getOpacity());
-                System.out.println("Saturation = " + color.getSaturation());
+//                System.out.println("\nPixel color at coordinates (" + readX + "," + readY + ") " + color.toString());
+//                System.out.println("R = " + color.getRed());
+//                System.out.println("G = " + color.getGreen());
+//                System.out.println("B = " + color.getBlue());
+//                System.out.println("Opacity = " + color.getOpacity());
+//                System.out.println("Saturation = " + color.getSaturation());
+                for (int i = 0; i < corresp.length; i++) {
+					// marge +-5
+                	if(corresp[i].getAverageValue() < color.getRed()) {
+                		
+                	}
+				}
             }
         }
 		
